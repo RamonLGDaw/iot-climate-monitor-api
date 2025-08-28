38283664f6310254a5e0d13dfe6938d8f9022fc6
RamonLGDaw <ramonlg.daw@gmail.com>
2025-08-28 19:51:45 +0200

    add CORS

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from routers.sensor_data_router import sensor
 from sqlalchemy import text
 from config.db import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "*",
+  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(sensor)
 
